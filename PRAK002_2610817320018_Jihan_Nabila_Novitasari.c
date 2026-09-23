b7292261d19b85943b0475c5a90f84eb167bf151
Dan <jihannabilanovitasari@gmail.com>
2026-09-23 23:08:07 +0800

    Soal 2
    Soal 2

---
+++ PRAK002_2610817320018_Jihan_Nabila_Novitasari.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Selamat Pagi, Jihan");
+    printf("Selamat Siang, Jihan");
+    printf("Selamat Malam, Jihan");
+
+    return 0;
+}
---
+++ PRAK002_2610817320018_Jihan_Nabila_Novitasari.py
@@ -0,0 +1,3 @@
+print("Selamat Pagi, Jihan")
+print("Selamat Siang, Jihan")
+print("Selamat Malam, Jiham")
