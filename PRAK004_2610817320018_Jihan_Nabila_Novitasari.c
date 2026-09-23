478261971cf648275eda173e06efd9122e863fcb
Dan <jihannabilanovitasari@gmail.com>
2026-09-23 23:24:15 +0800

    Soal 4

---
+++ PRAK004_2610817320018_Jihan_Nabila_Novitasari.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#            Jihan           #\n");
+    printf("#        2610817320018       #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+
+    return 0;
+}
+
